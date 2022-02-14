b5dfe15dd8d48b490c162e7f418c56bc7ffb92b5
Joao Henrique <joaohenrique.reis18@gmail.com>
2022-02-13 21:40:56 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/jh/cursomc/services/CategoriaService.java
+++ src/main/java/com/jh/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.jh.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,31 +17,35 @@ public class CategoriaService {
 
 	@Autowired // essa annotation é utilizada para instaciar qualquer dependencia pelo Spring
 	private CategoriaRepository repo;
-	
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos cadastrados!");
 		}
+
 	}
+	
+	public List<Categoria>findAll(){
+		return repo.findAll();
+	}
+	
 }
